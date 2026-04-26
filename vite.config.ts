6ffe9ecb462da70cda3dee0aed122abe2ac2cec8
= <=>
2026-04-26 23:00:56 +1000

    styling improvements

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      //creates an endpoint at http://localhost:5173/abs-api
-      '/abs-api': {
-        target: 'https://api.data.abs.gov.au/rest',
-        changeOrigin: true,
-        secure: true,
-        // removes '/abs-api' from the URL before sending it to the ABS
-        rewrite: (path) => path.replace(/^\/abs-api/, ''),
-      },
-    },
-  },
 })
